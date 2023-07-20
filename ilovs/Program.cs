95a875273da10b534619d4f84f2b7700e3331567
cambriner <cameron@yourcase.works>
2023-07-19 20:44:58 -0500

    Add project files.

---
+++ ilovs/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ilovs {
+
+    internal class Program {
+
+
+        private static void Main(string[] args) {
+
+            Synctor.ManualSync("sync/origin/source.json", "sync/target/target.json");
+        }
+    }
+}
---
+++ ilovs/Synctor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ilovs {
+
+    public class Synctor {
+
+        public Synctor() {
+
+        }
+
+        public static void ManualSync(string originFilePath, string targetFilePath) {
+
+            string originFile = File.ReadAllText(originFilePath);
+
+            
+
+        }
+    }
+}
