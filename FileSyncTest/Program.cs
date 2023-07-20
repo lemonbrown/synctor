4029a398634def3910ff46e085ad8c8aed4ee730
lemonbrown <cambriner@gmail.com>
2023-07-19 22:25:02 -0500

    init

---
+++ FileSyncTest/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using synctor;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+Synctor.ManualSync<Source>(
+    "C:\\Users\\cambr\\Documents\\syncs\\origin\\source.json", 
+    "C:\\Users\\cambr\\Documents\\syncs\\target\\source.json", 
+    (documents) =>
+{
+    
+    return documents;
+});
+
+
+class Source
+{
+    [JsonPropertyName("prop")]
+    public string Prop { get; set; } = "";
+}
---
+++ synctor/Synctor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace synctor
+{
+    public class Synctor
+    {
+
+        public static void ManualSync(string originFilePath, string targetFilePath, Func<JsonDocument, string>? handler = null)
+        {
+            var originFileText = File.ReadAllText(originFilePath);
+
+            if(originFileText?.Length > 0)
+            {
+             
+                if(handler != null)
+                {
+                    JsonDocument documents = JsonDocument.Parse(originFileText);
+
+                    _ = handler(documents);
+                }
+            }
+        }
+
+        public static void ManualSync<T>(string originFilePath, string targetFilePath, Func<List<T>, List<T>>? handler = null, bool insertIfNotExists = false)
+        {
+            var originFileText = File.ReadAllText(originFilePath);
+
+            if (originFileText?.Length > 0)
+            {
+
+                if (handler != null)
+                {
+                    List<T>? documents = JsonSerializer.Deserialize<List<T>>(originFileText);
+
+                    if (documents?.Count > 0) { 
+
+                        List<T> transformedDocuments = handler(documents);
+
+                        var targetExists = File.Exists(targetFilePath);
+
+                        if(insertIfNotExists && !targetExists)
+                        {
+
+                            File.WriteAllText(targetFilePath, JsonSerializer.Serialize(transformedDocuments));
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
